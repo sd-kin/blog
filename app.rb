94eea2fc5f489fdb1ec1dc41165fcdd3f24c25ea
sd-kin <sd-kin@rambler.ru>
2015-08-02 00:36:29 +0300

    add form for new post

--- app.rb
+++ app.rb
@@ -5,3 +5,11 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/newpost' do
+erb :newpost
+end
+
+get '/posts' do
+erb :posts
+end
